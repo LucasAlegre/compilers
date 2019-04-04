a770cc7a6cff62de14c9a97bea9b4f47ba4b8d66
Lucas Alegre <lucasnale@gmail.com>
2019-04-04 17:08:02 -0300

    Etapa 2 Grammar

--- etapa2/main.c
+++ etapa2/main.c
@@ -1,72 +1,41 @@
 /* 
-	ETAPA 1 - Compiladores - 2019/1 Turma A - Prof. Marcelo Johann
+	ETAPA 2 - Compiladores - 2019/1 Turma A - Prof. Marcelo Johann
 	Alunos:
 		Guilherme Haetinger e Lucas Alegre 
 */
 
 #include <stdio.h>
-#include "lex.yy.h"
-#include "hash.h"
+#include <stdlib.h>
 
-int yylex();
-extern char *yytext;
-extern FILE *yyin;
+extern FILE* yyin;
+extern FILE* file();
+extern int isRunning();
+extern int getLineNumber();
+extern int yyparse();
+extern void initMe();
+extern void hashPrint();
 
-int getLineNumber();
-int isRunning(void);
-void initMe(void);
+int main(int argc, char *argv[]){
 
-
-int main(int argc, char** argv){
-
-	int tok;
-
-	if (argc < 2){
-		printf("call: ./etapa1 input.txt\n");
-		exit(1);
-    }
-  	if (0 == (yyin = fopen(argv[1], "r"))){
-		printf("Cannot open file %s... \n",argv[1]);
-		exit(1);
-    }
-
-	initMe();
-	while(isRunning()){
-
-		tok = yylex();
-		if (!isRunning())
-      		break;
-
-		switch(tok){
-			case KW_BYTE: fprintf(stderr, "KW_BYTE na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-			case KW_INT:  fprintf(stderr, "KW_INT na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_FLOAT:fprintf(stderr, "KW_FLOAT na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_IF:   fprintf(stderr, "KW_IF na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_THEN: fprintf(stderr, "KW_THEN na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_ELSE: fprintf(stderr, "KW_ELSE na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_LOOP: fprintf(stderr, "KW_LOOP na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_LEAP: fprintf(stderr, "KW_LEAP na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_READ: fprintf(stderr, "KW_READ na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_RETURN: fprintf(stderr, "KW_RETURN na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case KW_PRINT: fprintf(stderr, "KW_PRINT na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_LE: fprintf(stderr, "OPERATOR_LE na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_GE: fprintf(stderr, "OPERATOR_GE na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_EQ: fprintf(stderr, "OPERATOR_EQ na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_DIF: fprintf(stderr, "OPERATOR_DIF na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_OR:  fprintf(stderr, "OPERATOR_OR na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_AND: fprintf(stderr, "OPERATOR_AND na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case OPERATOR_NOT: fprintf(stderr, "OPERATOR_NOT na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case TK_IDENTIFIER: fprintf(stderr, "TK_IDENTIFIER na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case LIT_INTEGER: fprintf(stderr, "LIT_INTEGER na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case LIT_FLOAT: fprintf(stderr, "LIT_FLOAT na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case LIT_STRING: fprintf(stderr, "LIT_STRING na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-			case LIT_CHAR: fprintf(stderr, "LIT_CHAR na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-   			case TOKEN_ERROR: fprintf(stderr, "TOKEN_ERROR na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-			default: fprintf(stderr, "Caracter especial na linha %d. - tok: %d \n", getLineNumber(), tok); break;
-		}
+  int token;
+	if(argc < 2){
+		fprintf(stderr,"Execute: ./etapa2 nomearquivo.\n");
+		return 1;
 	}
-
+	if(!(file(argv[1]))){
+		fprintf(stderr,"Nao eh possivel abrir o arquivo %s.\n",argv[1]);
+		return 2;
+	}
+	
+	initMe();
+	
+	while(isRunning()){
+		yyparse();
+	}	
+	
 	hashPrint();
 
-	return 0;
+	fprintf(stderr,"Compiled Successfully\n");
+
+  return 0; 
 }
--- etapa2/parser.y
+++ etapa2/parser.y
@@ -37,17 +37,130 @@
 %token LIT_FLOAT     
 %token LIT_CHAR      
 %token LIT_STRING    
-%token TOKEN_ERROR   
+%token TOKEN_ERROR
+
+%left '<' '>'
+%left OPERATOR_LE OPERATOR_GE OPERATOR_EQ OPERATOR_DIF OPERATOR_OR OPERATOR_AND OPERATOR_NOT
+%left '+' '-'
+%left '*' '/'
+
+
+%start program
 
 %%
 
-prog:
+program : l_declarations
+    		;
+
+l_declarations : declaration l_declarations            
+							 |
+							 ;
+
+declaration : tipo TK_IDENTIFIER '=' literal ';'
+						| tipo TK_IDENTIFIER '[' LIT_INTEGER ']' init_vector ';'
+						| tipo TK_IDENTIFIER '(' l_param ')' bloco ';'
+						;
+
+tipo : KW_BYTE
+     | KW_INT
+     | KW_FLOAT
+     ;
+
+literal : LIT_INTEGER
+        | LIT_FLOAT
+        | LIT_CHAR
+        ;
+
+init_vector :
+            | ':' literal vector_elements
+            ;
+
+vector_elements : 
+                | literal vector_elements
+                ;
+
+l_param : 
+        | param param_end
+        ;
+
+param_end :
+					| ',' param param_end
+          ;
+
+param : tipo TK_IDENTIFIER
+      ;
+
+bloco : '{' l_cmd '}'
+      ;
+
+l_cmd : 
+			| cmd l_cmd_end
+			;
+
+l_cmd_end : 
+          | ';' cmd l_cmd_end
+          ;
+
+cmd :
+    | bloco
+    | TK_IDENTIFIER '=' expression
+    | TK_IDENTIFIER '[' expression ']' '=' expression
+    | KW_READ TK_IDENTIFIER
+    | KW_PRINT l_print
+    | KW_RETURN expression
+    | KW_IF '(' expression ')' KW_THEN cmd else
+    | KW_LOOP '(' expression ')' cmd
+    | KW_LEAP
     ;
+
+else :
+     | KW_ELSE cmd
+     ;
+
+expression : TK_IDENTIFIER
+           | TK_IDENTIFIER '[' expression ']'
+           | TK_IDENTIFIER '(' l_args ')'
+           | literal
+           | expression '+' expression
+           | expression '-' expression
+           | expression '*' expression
+           | expression '/' expression
+           | expression '<' expression
+					 | expression '>' expression
+					 | expression OPERATOR_EQ expression  
+           | expression OPERATOR_GE expression
+           | expression OPERATOR_LE expression
+					 | expression OPERATOR_DIF expression
+           | expression OPERATOR_AND expression
+           | expression OPERATOR_OR expression   
+					 | OPERATOR_NOT expression
+					 | '(' expression ')' 
+	         ;
+
+l_args : 
+       | expression l_args_end
+       ;
+
+l_args_end :
+           | ',' expression l_args_end
+           ;
+
+l_print : 
+        | element_print l_print_end
+        ;
+ 
+l_print_end :
+            | ',' element_print l_print_end
+            ;
+
+element_print : LIT_STRING
+              | expression
+              ; 
 
 %%
 
 int yyerror(char *err){
 	
-	fprintf(stderr, "ERRO na linha = %d\n", getLineNumber());
+	fprintf(stderr, "ERROR in line = %d\n", getLineNumber());
 	exit(3);
 }
